4dedba89aad29baf4b1c813b06292a12024d9614
boxlerr <boxlerjulian@hotmail.com>
2024-05-13 12:11:53 -0300

    readme
    readme, archivos para conectar base de datos

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,36 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class DatabaseConnection {
+   private static final String URL = "jdbc:mysql://localhost:3306/prueba";
+   private static final String USER = "root";
+   private static final String PASSWORD = "";
+    
+    private static DatabaseConnection instance;
+    
+    private Connection connection;
+    
+    //genera la bdd cuanto usa el constructor
+    private DatabaseConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+    
+    public Connection getConnection() {
+        return connection;
+    }
+}
---
+++ src/UserRepository.java
@@ -0,0 +1,16 @@
+
+
+import java.util.List;
+
+public interface UserRepository {
+	//prototipos de metodos 
+    List<Usuario> getAllUsers(); // llama a todos los usuarios de la bdd
+    
+    Usuario getUserById(int id); //llama solo a uno, por su id
+    
+    void addUser(Usuario user); //añade usuarios a la bdd
+    
+    void updateUser(Usuario user); //actualiza los usuarios de la bdd
+    
+    void deleteUser(int id); //eliminar usuarios de la bdd
+}
