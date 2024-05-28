6f6479dbcd98298ca39faf4606fc8d80d87f80a7
boxlerr <boxlerjulian@hotmail.com>
2024-05-28 18:16:50 -0300

    boludeces prestamo
    dsp sigo me re pudri

--- src/controlador/PrestamoControlador.java
+++ src/controlador/PrestamoControlador.java
@@ -2,10 +2,14 @@ package controlador;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import interfaces.PrestamoRepository;
+import modelos.*;
 
 
 public class PrestamoControlador implements PrestamoRepository {
@@ -18,38 +22,76 @@ public class PrestamoControlador implements PrestamoRepository {
 
     @Override
     public void realizarPrestamo(int idLibro, int idUsuario, int idSucursal) {
-
         try {
+            connection.setAutoCommit(false); 
             PreparedStatement ps = connection.prepareStatement("INSERT INTO prestamos (idLibro, idUsuario, idSucursal_fk, fechaPrestamo) VALUES (?, ?, ?, NOW())");
             ps.setInt(1, idLibro);
             ps.setInt(2, idUsuario);
             ps.setInt(3, idSucursal);
             ps.executeUpdate();
+
+            PreparedStatement psUpdate = connection.prepareStatement("UPDATE libro SET stock = stock - 1 WHERE idLibro = ?");
+            psUpdate.setInt(1, idLibro);
+            psUpdate.executeUpdate();
+
+            connection.commit();
         } catch (SQLException ex) {
-            Logger.getLogger(PrestamoControlador.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
             try {
-            	connection.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(PrestamoControlador.class.getName()).log(Level.SEVERE, null, ex);
+                connection.rollback(); 
+            } catch (SQLException e) {
+                Logger.getLogger(PrestamoControlador.class.getName()).log(Level.SEVERE, null, e);
             }
+            Logger.getLogger(PrestamoControlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    @Override
+    public void devolverLibro(int idPrestamo) {
+        try {
+            connection.setAutoCommit(false); 
+
+            
+            PreparedStatement psSelect = connection.prepareStatement("SELECT idLibro FROM prestamos WHERE idPrestamo = ?");
+            psSelect.setInt(1, idPrestamo);
+            ResultSet rs = psSelect.executeQuery();
+            if (rs.next()) {
+                int idLibro = rs.getInt("idLibro");
+
+                
+                PreparedStatement ps = connection.prepareStatement("UPDATE prestamos SET fechaDevolucion = NOW() WHERE idPrestamo = ?");
+                ps.setInt(1, idPrestamo);
+                ps.executeUpdate();
+
+                
+                PreparedStatement psUpdate = connection.prepareStatement("UPDATE libro SET stock = stock + 1 WHERE idLibro = ?");
+                psUpdate.setInt(1, idLibro);
+                psUpdate.executeUpdate();
+
+                connection.commit();
+            }
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                Logger.getLogger(PrestamoControlador.class.getName()).log(Level.SEVERE, null, e);
+            }
+            Logger.getLogger(PrestamoControlador.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     @Override
-    public void devolverLibro(int idPrestamo) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("UPDATE prestamos SET fechaDevolucion = NOW() WHERE idPrestamo = ?");
-            ps.setInt(1, idPrestamo);
-            ps.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(PrestamoControlador.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-            	connection.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(PrestamoControlador.class.getName()).log(Level.SEVERE, null, ex);
+    public List<Prestamos> obtenerListaPrestamos() {
+        List<Prestamos> listaPrestamos = new ArrayList<>();
+        try (PreparedStatement stmt = connection.prepareStatement("SELECT * FROM prestamos")) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Prestamos prestamo = new Prestamos(rs.getInt("idPrestamo"),rs.getInt("idLibro"),rs.getInt("idUsuario"),rs.getInt("idSucursal_fk"),rs.getDate("fechaPrestamo").toLocalDate(), rs.getDate("fechaDevolucion") != null ? rs.getDate("fechaDevolucion").toLocalDate() : null
+                );
+                listaPrestamos.add(prestamo);
             }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
+        return listaPrestamos;
     }
-}
+  }
+
--- src/interfaces/PrestamoRepository.java
+++ src/interfaces/PrestamoRepository.java
@@ -1,7 +1,12 @@
 package interfaces;
 
+import java.util.List;
+
+import modelos.Prestamos;
+
 public interface  PrestamoRepository {
 	
 	void realizarPrestamo(int idLibro, int idUsuario, int idSucursal);
 	void devolverLibro(int idPrestamo);
+	List<Prestamos> obtenerListaPrestamos();
 }
