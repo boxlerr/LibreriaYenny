d935c338f19e3f61f116374c5269ad6c288a16ef
boxlerr <boxlerjulian@hotmail.com>
2024-05-30 02:52:29 -0300

    agregue test
    no iba a irme a dormir sin agregar algun test

--- src/test/TestEmpleado.java
+++ src/test/TestEmpleado.java
@@ -1,1 +1,35 @@
-//tengo sueño
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import controlador.VentasControlador;
+
+
+public class TestEmpleado {
+	@Test
+	public void verificarDisponibilidadLibroTest() throws SQLException {
+	    VentasControlador controlador = new VentasControlador();
+
+	    boolean flag = false;
+	    if (controlador.verificarDisponibilidadLibro(2)) {
+	        flag = true;
+	    }
+	    assertEquals(true, flag);
+	}
+
+@Test
+public void verificarNoDisponibilidadLibroTest() throws SQLException {
+    VentasControlador controlador = new VentasControlador();
+ 
+    boolean flag = false;
+    if (!controlador.verificarDisponibilidadLibro(7857)) { 
+        flag = true;
+    }
+    assertEquals(true, flag);
+}
+
+//tengo sueño no hago mas
+}
