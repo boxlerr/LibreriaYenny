3e41fd3cdbec96b789902095445b3574e176ab8d
ErosB8 <eros0088@hotmail.com>
2024-06-03 11:23:46 -0300

    Borrar coments

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -34,11 +34,6 @@ class Main {
 			switch (inicio) {
 			case 0:
 				JOptionPane.showMessageDialog(null, "Iniciar sesion");
-//				String[] usuarios = new String[usuarioControlador.getAllUsers().size()];
-//				for (int i = 0; i < usuarios.length; i++) {
-//					usuarios[i] = usuarioControlador.getAllUsers().get(i).toString();
-//				}
-//				JOptionPane.showMessageDialog(null, usuarios);
 				String mail = JOptionPane.showInputDialog("Ingrese su mail");
 				String contraseña = JOptionPane.showInputDialog("Ingrese su contraseña");
 				
@@ -92,63 +87,6 @@ class Main {
 					
 			}
 		} while (inicio!=2);
-		
-
-		
-//		do {
-//			
-//			inicio = JOptionPane.showOptionDialog(null, "Bienvenido a la librería", "Librería", 0, 0, null, opciones, opciones[0]);
-//				
-//			switch (inicio) {
-//			case 0:
-//				
-//				JOptionPane.showMessageDialog(null, "Ingresa como gerente");
-//				String usuario1 = JOptionPane.showInputDialog("Ingrese su nombre de usuario");
-//				String ape1 = JOptionPane.showInputDialog("Ingrese su apellido");
-//				String cuit = JOptionPane.showInputDialog("Ingrese su cuit");
-//				
-//				Gerente gerente = new Gerente (usuario1, ape1, cuit, 0);
-//				gerente.Ingreso(cuit, biblioteca, ape1);
-//				
-//				break;
-//				
-//			case 1:
-//				//login admin: admin - 1234
-//				JOptionPane.showMessageDialog(null, "Ingresa como empleado");
-//				String ape = JOptionPane.showInputDialog("Ingrese su apellido");
-//				
-//				int intentos=3;
-//				
-//					do {
-//						String usuario = JOptionPane.showInputDialog("Ingrese nombre de usuario");
-//						String contraseña =JOptionPane.showInputDialog("Ingrese contraseña");
-//						if (contraseña.equals("1234")&&usuario.equals("admin")) {
-//							Empleado empleado = new Empleado (usuario, ape, contraseña);
-//							empleado.Ingreso(contraseña, biblioteca, ape);
-//							intentos=0;
-//						} else {
-//							intentos--;
-//							JOptionPane.showMessageDialog(null, "Nombre de usuario o contraseña incorrecta \nIntentos restantes " + intentos);
-//						}
-//					} while (intentos>0);
-//				break;
-//				
-//			case 2:
-//				JOptionPane.showMessageDialog(null, "Ingresa como cliente");
-//				String nombre2 = JOptionPane.showInputDialog("Ingrese su nombre");
-//				String ape2 = JOptionPane.showInputDialog("Ingrese su apellido");
-//				String indentificador2 =JOptionPane.showInputDialog("Ingrese su indentificador");
-//				Cliente cliente = new Cliente (nombre2, ape2, indentificador2);
-//				cliente.Ingreso(indentificador2, biblioteca, ape2);
-//				break;
-//				
-//			case 3:
-//				JOptionPane.showMessageDialog(null, "Préstamos realizados:\n" + biblioteca.getPrestamos());
-//				JOptionPane.showMessageDialog(null, "Libros disponibles:\n" + biblioteca.getLibrosdisponibles());
-//				JOptionPane.showMessageDialog(null, "Se cierra el sistema por hoy, que tenga buena noche!");
-//				break;
-//			}
-//		} while (inicio!=3);
 
 	}
 
