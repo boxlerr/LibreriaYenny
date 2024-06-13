4bfd8d37d29cd4d942005195b3f5bb746f5d2278
diegonzz <diego.nunez@davinci.edu.ar>
2024-06-12 23:33:37 -0300

    Validaciones Usuarios

--- src/vista/Agregar.java
+++ src/vista/Agregar.java
@@ -16,6 +16,7 @@ import javax.swing.JButton;
 
 import controlador.*;
 import modelos.*;
+import java.awt.Font;
 
 public class Agregar extends JFrame {
 
@@ -23,8 +24,14 @@ public class Agregar extends JFrame {
 	private JPanel contentPane;
 	private UsuarioControlador controlador = new UsuarioControlador();
 	private JTextField InpMail;
-	private JTextField InpContrasena;
 	private JTextField InpTipo;
+	private JTextField textField;
+	private JTextField inpNombre;
+	private JTextField inpApellido;
+	private JTextField inpDni;
+	private JTextField inpMail;
+	private JTextField inpContrasena;
+	private JTextField inpTipo;
 
 	public Agregar() {
 		this(new Usuario());
@@ -33,91 +40,286 @@ public class Agregar extends JFrame {
 	public Agregar(Usuario usuario) {
 		this.setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 456, 318);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblIngreseMail = new JLabel("Ingrese mail del usuario:");
-		lblIngreseMail.setBounds(10, 35, 164, 14);
-		contentPane.add(lblIngreseMail);
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblNombre.setBounds(46, 34, 45, 13);
+		contentPane.add(lblNombre);
 		
-		InpMail = new JTextField();
-		InpMail.setBounds(11, 58, 131, 20);
-		contentPane.add(InpMail);
-		InpMail.setColumns(10);
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblApellido.setBounds(242, 34, 51, 13);
+		contentPane.add(lblApellido);
 		
-		JLabel lblIngreseContrasena = new JLabel("Ingrese contraseña del usuario:");
-		lblIngreseContrasena.setBounds(10, 109, 228, 14);
-		contentPane.add(lblIngreseContrasena);
+		JLabel lblDni = new JLabel("DNI");
+		lblDni.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblDni.setBounds(46, 88, 45, 13);
+		contentPane.add(lblDni);
 		
-		InpContrasena = new JTextField();
-		InpContrasena.setColumns(10);
-		InpContrasena.setBounds(11, 129, 131, 20);
-		contentPane.add(InpContrasena);
+		JLabel lblEmail = new JLabel("E-Mail");
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblEmail.setBounds(242, 88, 45, 13);
+		contentPane.add(lblEmail);
 		
-		JLabel lblIngreseTipo = new JLabel("Ingrese tipo del usuario:");
-		lblIngreseTipo.setBounds(10, 191, 164, 14);
-		contentPane.add(lblIngreseTipo);
+		JLabel lblContrasena = new JLabel("Contraseña");
+		lblContrasena.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblContrasena.setBounds(46, 142, 62, 13);
+		contentPane.add(lblContrasena);
 		
-		InpTipo = new JTextField();
-		InpTipo.setColumns(10);
-		InpTipo.setBounds(11, 214, 131, 20);
-		contentPane.add(InpTipo);
+		JLabel lblErrorMailExistente = new JLabel("Ya existe una cuenta con este mail");
+		lblErrorMailExistente.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorMailExistente.setForeground(new Color(255, 0, 0));
+		lblErrorMailExistente.setBounds(273, 88, 165, 13);
+		contentPane.add(lblErrorMailExistente);
+		lblErrorMailExistente.setVisible(false);
 		
-		JLabel lblErrorMail = new JLabel("Ingrese un mail válido");
-		lblErrorMail.setForeground(new Color(255, 0, 0));
-		lblErrorMail.setBounds(157, 61, 146, 14);
-		lblErrorMail.setVisible(false);
-		contentPane.add(lblErrorMail);
+		JLabel lblErrorMailInvalido;
+		lblErrorMailInvalido = new JLabel("Ingrese un mail válido");
+		lblErrorMailInvalido.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorMailInvalido.setForeground(new Color(255, 0, 0));
+		lblErrorMailInvalido.setBounds(273, 88, 165, 13);
+		contentPane.add(lblErrorMailInvalido);
+		lblErrorMailInvalido.setVisible(false);
 		
-		JLabel lblErrorContrasena = new JLabel("Ingrese una contraseña válida");
-		lblErrorContrasena.setForeground(Color.RED);
-		lblErrorContrasena.setBounds(154, 132, 193, 14);
-		lblErrorContrasena.setVisible(false);
-		contentPane.add(lblErrorContrasena);
+		JLabel lblErrorContrasenaNumeros = new JLabel("La contraseña debe tener números");
+		lblErrorContrasenaNumeros.setForeground(new Color(255, 0, 0));
+		lblErrorContrasenaNumeros.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorContrasenaNumeros.setBounds(46, 187, 148, 13);
+		contentPane.add(lblErrorContrasenaNumeros);
+		lblErrorContrasenaNumeros.setVisible(false);
+		
+		JLabel lblErrorContrasenaCaracteres = new JLabel("La contraseña debe contener al menos 6 caracteres");
+		lblErrorContrasenaCaracteres.setForeground(new Color(255, 0, 0));
+		lblErrorContrasenaCaracteres.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorContrasenaCaracteres.setBounds(18, 187, 213, 13);
+		contentPane.add(lblErrorContrasenaCaracteres);
+		lblErrorContrasenaCaracteres.setVisible(false);
+		
+		JLabel lblErrorContrasenaBlank;
+		lblErrorContrasenaBlank = new JLabel("Ingrese una contraseña");
+		lblErrorContrasenaBlank.setForeground(new Color(255, 0, 0));
+		lblErrorContrasenaBlank.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorContrasenaBlank.setBounds(101, 142, 101, 13);
+		contentPane.add(lblErrorContrasenaBlank);
+		lblErrorContrasenaBlank.setVisible(false);
+		
+		JLabel lblErrorNoCoinciden;
+		lblErrorNoCoinciden = new JLabel("No coinciden las contraseñas");
+		lblErrorNoCoinciden.setForeground(new Color(255, 0, 0));
+		lblErrorNoCoinciden.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorNoCoinciden.setBounds(254, 187, 124, 13);
+		contentPane.add(lblErrorNoCoinciden);
+		lblErrorNoCoinciden.setVisible(false);
+		
+		JLabel lblErrorApellido = new JLabel("Ingrese un apellido");
+		lblErrorApellido.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorApellido.setForeground(new Color(255, 0, 0));
+		lblErrorApellido.setBounds(282, 34, 108, 13);
+		contentPane.add(lblErrorApellido);
+		lblErrorApellido.setVisible(false);
+		
+		JLabel lblErrorApellidoNumeros = new JLabel("Ingrese un apellido válido");
+		lblErrorApellidoNumeros.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorApellidoNumeros.setForeground(new Color(255, 0, 0));
+		lblErrorApellidoNumeros.setBounds(282, 34, 116, 13);
+		contentPane.add(lblErrorApellidoNumeros);
+		lblErrorApellidoNumeros.setVisible(false);
+		
+		JLabel lblErrorNombre = new JLabel("Ingrese un nombre");
+		lblErrorNombre.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorNombre.setForeground(new Color(255, 0, 0));
+		lblErrorNombre.setBounds(86, 34, 108, 13);
+		contentPane.add(lblErrorNombre);
+		lblErrorNombre.setVisible(false);
+		
+		JLabel lblErrorNombreNumeros = new JLabel("Ingrese un nombre válido");
+		lblErrorNombreNumeros.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorNombreNumeros.setForeground(new Color(255, 0, 0));
+		lblErrorNombreNumeros.setBounds(86, 34, 116, 13);
+		contentPane.add(lblErrorNombreNumeros);
+		lblErrorNombreNumeros.setVisible(false);
+		
+		JLabel lblErrorDNIINvalido = new JLabel("Ingrese un DNI válido");
+		lblErrorDNIINvalido.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorDNIINvalido.setForeground(new Color(255, 0, 0));
+		lblErrorDNIINvalido.setBounds(70, 88, 124, 13);
+		contentPane.add(lblErrorDNIINvalido);
+		lblErrorDNIINvalido.setVisible(false);
+
+		inpNombre = new JTextField();
+		inpNombre.setBounds(46, 48, 148, 29);
+		contentPane.add(inpNombre);
+		inpNombre.setColumns(10);
+		
+		inpApellido = new JTextField();
+		inpApellido.setColumns(10);
+		inpApellido.setBounds(242, 48, 148, 29);
+		contentPane.add(inpApellido);
+		
+		inpDni = new JTextField();
+		inpDni.setColumns(10);
+		inpDni.setBounds(46, 102, 148, 29);
+		contentPane.add(inpDni);
+		
+		inpMail = new JTextField();
+		inpMail.setColumns(10);
+		inpMail.setBounds(242, 102, 148, 29);
+		contentPane.add(inpMail);
+		
+		inpContrasena = new JTextField();
+		inpContrasena.setColumns(10);
+		inpContrasena.setBounds(46, 160, 148, 29);
+		contentPane.add(inpContrasena);
+		
+		inpTipo = new JTextField();
+		inpTipo.setColumns(10);
+		inpTipo.setBounds(242, 160, 148, 29);
+		contentPane.add(inpTipo);
+		
+		JLabel lblTipo = new JLabel("Tipo");
+		lblTipo.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblTipo.setBounds(242, 141, 45, 13);
+		contentPane.add(lblTipo);
 		
 		JLabel lblErrorTipo = new JLabel("Ingrese un tipo válido");
-		lblErrorTipo.setForeground(Color.RED);
-		lblErrorTipo.setBounds(154, 217, 149, 14);
+		lblErrorTipo.setForeground(new Color(255, 0, 0));
+		lblErrorTipo.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblErrorTipo.setBounds(273, 141, 117, 14);
 		lblErrorTipo.setVisible(false);
 		contentPane.add(lblErrorTipo);
 		
+		JLabel lblAadirCuenta = new JLabel("Añadir cuenta");
+		lblAadirCuenta.setFont(new Font("Arial", Font.BOLD, 15));
+		lblAadirCuenta.setBounds(163, 11, 124, 13);
+		contentPane.add(lblAadirCuenta);
+		
+		JLabel lblErrorDNI = new JLabel("Ingrese un DNI válido");
+		lblErrorDNI.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		lblErrorDNI.setForeground(new Color(255, 0, 0));
+		lblErrorDNI.setBounds(70, 88, 124, 13);
+		contentPane.add(lblErrorDNI);
+		lblErrorDNI.setVisible(false);
+		
 		JButton btnAgregar = new JButton("Agregar");
-		btnAgregar.setBounds(335, 85, 89, 39);
+		btnAgregar.setBounds(76, 211, 89, 45);
 		btnAgregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String mails="";
-				for (int i = 0; i < controlador.getAllUsers().size(); i++) {
-					mails = mails + controlador.getAllUsers().get(i).getMail();
+				boolean flagMailExistencia = controlador.verificarMailExistencia(inpMail.getText());
+				boolean flagMailValido = controlador.verificarMailValido(inpMail.getText());
+				if (flagMailValido) {
+					if (flagMailExistencia) {
+						lblErrorMailInvalido.setVisible(false);
+						lblErrorMailExistente.setVisible(false);
+					} else {
+						lblErrorMailInvalido.setVisible(false);
+						lblErrorMailExistente.setVisible(true);
+					}
+				} else {
+					lblErrorMailExistente.setVisible(false);
+					lblErrorMailInvalido.setVisible(true);
 				}
 				
+				boolean flagContrasenaCaracteres = controlador.verificarContraseñaCaracteres(inpContrasena.getText());
+				boolean flagContrasenaNumeros = controlador.verificarNumeros(inpContrasena.getText());
+				boolean flagContrasenaBlank = true;
 				
-				if (!InpMail.getText().contains("@") || !InpMail.getText().contains(".")) {
-					lblErrorMail.setVisible(true);
-					
-				} else if (InpContrasena.getText().length()<6) {
-					lblErrorContrasena.setVisible(true);
-					
-				} else if (!InpTipo.getText().equalsIgnoreCase("Gerente") && !InpTipo.getText().equalsIgnoreCase("Empleado") && !InpTipo.getText().equalsIgnoreCase("Escritor")) {
-					lblErrorTipo.setVisible(true);
-					
+				if (inpContrasena.getText().isBlank()) {
+					flagContrasenaBlank = false;
+				}
+				
+				if (flagContrasenaBlank) {
+					if (flagContrasenaCaracteres) {
+						if (flagContrasenaNumeros) {
+							lblErrorContrasenaBlank.setVisible(false);
+							lblErrorContrasenaCaracteres.setVisible(false);
+							lblErrorContrasenaNumeros.setVisible(false);
+						} else {
+							lblErrorContrasenaBlank.setVisible(false);
+							lblErrorContrasenaCaracteres.setVisible(false);
+							lblErrorContrasenaNumeros.setVisible(true);
+						}
+					} else {
+						lblErrorContrasenaBlank.setVisible(false);
+						lblErrorContrasenaNumeros.setVisible(false);
+						lblErrorContrasenaCaracteres.setVisible(true);
+					}
 				} else {
-					usuario.setMail(InpMail.getText());
-					usuario.setContraseña(InpContrasena.getText());
-					usuario.setTipo(InpTipo.getText());
-					controlador.addUser(usuario);
+					lblErrorContrasenaNumeros.setVisible(false);
+					lblErrorContrasenaCaracteres.setVisible(false);
+					lblErrorContrasenaBlank.setVisible(true);
+				}		
+				
+				if (inpNombre.getText().isBlank()) {
+					lblErrorNombre.setVisible(true);
+				} else {
+					lblErrorNombre.setVisible(false);
+					if (controlador.verificarNumeros(inpNombre.getText())==false) {
+						//Seguir
+						lblErrorNombreNumeros.setVisible(false);
+						lblErrorNombre.setVisible(false);
+					} else {
+						lblErrorNombreNumeros.setVisible(true);
+					}
+				}
+				
+				if (inpApellido.getText().isBlank()) {
+					lblErrorApellido.setVisible(true);
+				} else {
+					lblErrorApellido.setVisible(false);
+					if (controlador.verificarNumeros(inpApellido.getText())==false) {
+						//Seguir
+						lblErrorApellidoNumeros.setVisible(false);
+						lblErrorApellido.setVisible(false);
+					} else {
+						lblErrorApellidoNumeros.setVisible(true);
+					}
+				}
+				
+				boolean flagDNIValido = controlador.VerificarDNI(inpDni.getText());
+				
+				if (flagDNIValido) {
+					lblErrorDNIINvalido.setVisible(false);
+				} else {
+					lblErrorDNIINvalido.setVisible(true);
+				}
+				
+				if (flagMailExistencia && flagMailValido && flagContrasenaCaracteres && flagContrasenaNumeros && flagContrasenaBlank && 
+						!inpNombre.getText().isBlank() && controlador.verificarNumeros(inpNombre.getText())==false && !inpApellido.getText().isBlank() &&
+						controlador.verificarNumeros(inpApellido.getText())==false && flagDNIValido) {
+					controlador.addUser(new Usuario(inpMail.getText(), inpContrasena.getText(), inpTipo.getText()));
+					
+					if (inpTipo.getText().equalsIgnoreCase("Gerente")) {
+						GerenteControlador.addGerente(controlador.getUserById(inpMail.getText(), inpContrasena.getText()), new Gerente(inpMail.getText(), inpContrasena.getText(), "Gerente", inpNombre.getText(), inpApellido.getText(), Integer.parseInt(inpDni.getText())));
+					}
+					
+					if (inpTipo.getText().equalsIgnoreCase("Empleado")) {
+						GerenteControlador.addGerente(controlador.getUserById(inpMail.getText(), inpContrasena.getText()), new Empleado(inpMail.getText(), inpContrasena.getText(), "Empleado", inpNombre.getText(), inpApellido.getText(), Integer.parseInt(inpDni.getText())));
+					}
+					
+					if (inpTipo.getText().equalsIgnoreCase("Escritor")) {
+						EscritorControlador.addEscritor(controlador.getUserById(inpMail.getText(), inpContrasena.getText()), new Escritor(inpMail.getText(), inpContrasena.getText(), "Escritor", inpNombre.getText(), inpApellido.getText(), Integer.parseInt(inpDni.getText())));
+						
+					}
+					
 					dispose();
 					PantallaCuentas pantallaCuentas = new PantallaCuentas();
+					
 				}
-				}
+				
+				
+			}
 		});	
 		contentPane.add(btnAgregar);
 		
 		
 		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(272, 211, 89, 45);
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -126,10 +328,8 @@ public class Agregar extends JFrame {
 			}
 			
 		});
-		btnCancelar.setBounds(335, 151, 89, 45);
 		contentPane.add(btnCancelar);
 		
-		
-		
+
 	}
 }
